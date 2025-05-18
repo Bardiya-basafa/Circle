818a1e00bb43783b4c692d594d0bfe029cfcf209
bardiya <bardiya.basafa@gmail.com>
2025-05-18 21:22:58 +0330

    commit

--- Services/obj/Debug/net9.0/Services.AssemblyInfo.cs
+++ Services/obj/Debug/net9.0/Services.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8577d72ce36250fc1c603500102d322c16e443f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7026dc56756f8500af6a93b7780699b75d148c4d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
